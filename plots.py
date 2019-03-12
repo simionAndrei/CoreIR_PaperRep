fe7dce137d0870da18f4ef5f380544aaa3fd9b3e
simionAndrei <andrei.simion.c@gmail.com>
2019-03-12 20:40:57 +0100

    Update plots and add feats imp plot

--- plots.py
+++ plots.py
@@ -2,32 +2,51 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import pandas as pd
+from matplotlib import gridspec
 
-import matplotlib
-matplotlib.rcParams['font.weight']= 'bold'
+plt.rc('font', weight='bold')        
+plt.rc('axes', labelweight='bold')
+plt.rcParams.update({'font.size': 16})
 
 def make_tag_occurences_plot(occurences, plt_title, x_label, y_label, filename, logger, 
-  plot_tags = False, vertical_line = None, color = 'blue', edgecolor = False, log = False):
+  plot_tags = False, vertical_line = None, color = 'blue', edgecolor = None, log = False):
 
-  fig = plt.figure(figsize=(8, 8))
+  if plot_tags:
+    fig = plt.figure(figsize=(10, 10))
+  else:
+    fig = plt.figure(figsize=(12, 12))
+
   sns.set()
-  plt.bar(range(len(occurences)), [item[1] for item in occurences], width = 0.9, 
-    color = color, edgecolor = edgecolor, log = log)
 
-  plt.xlabel(x_label)
-  plt.ylabel(y_label)
+  if plot_tags:
+    plt.bar(range(len(occurences)), [item[1] for item in occurences], width = 0.9, 
+      color = color, edgecolor = edgecolor, log = log)
+  else:
+    gs = gridspec.GridSpec(1, 2, width_ratios=[1, 4]) 
+    ax0 = plt.subplot(gs[0])
+    ax0.bar(range(len(occurences)), [item[1] for item in occurences], width = 0.9, 
+      color = 'white', edgecolor = 'blue', log = False)
+    ax0.axvline(x = vertical_line, color = 'black')
+    ax1 = plt.subplot(gs[1])
+    ax1.bar(range(len(occurences)), [item[1] for item in occurences], width = 1.0, 
+      color = color, edgecolor = 'white', log = True)
+
+  plt.xlabel(x_label, fontsize = 18)
+  plt.ylabel(y_label, fontsize = 18)
   plt.title(plt_title)
+  plt.xticks(fontsize = 15)
+  plt.yticks(fontsize = 15)
 
   if plot_tags:
     x = np.array(range(len(occurences)))
     my_xticks = [item[0] for item in occurences]
-    plt.xticks(x, my_xticks, rotation = 90)
+    plt.xticks(x, my_xticks, rotation = 90, fontsize = 15)
 
   if vertical_line is not None:
     plt.axvline(x = vertical_line, color = 'black')
     plt.text(vertical_line + 2, 1500,'X=37', rotation=0)
 
-  plt.savefig(logger.get_output_file(filename), dpi = 120)
+  plt.savefig(logger.get_output_file(filename), dpi = 120, bbox_inches='tight')
   plt.close()
 
 
@@ -44,7 +63,6 @@ def make_accuracy_f1_plot(results_filename, plot_filename, logger):
 
   colors = ['blue', 'blue', 'blue', 'red', 'green', 'orange', 'dimgray', 'maroon', 'navy']
   markers = ['*', 'p', 'D', 'X', 'X', 'X', 'X', 'X', 'X']
-  print(colors)
   for i, model in enumerate(models):
     x = acc[i]
     y = f1[i]
@@ -56,3 +74,60 @@ def make_accuracy_f1_plot(results_filename, plot_filename, logger):
 
   plt.savefig(logger.get_output_file(plot_filename), dpi = 120,  bbox_inches='tight')
   plt.close()
+
+
+def make_feats_importance_barplot(feats_imp_filename, plot_filename, 
+  num_feats_to_plot, logger):
+
+  fig = plt.figure(figsize=(8, 10))
+  sns.set()
+
+  df = pd.read_csv(logger.get_output_file(feats_imp_filename))
+
+  model1_feats_imp_scores = df.iloc[0, 1:]
+  model2_feats_imp_scores = df.iloc[1, 1:]
+
+  model1_name_score_pairs = list(zip(df.columns[1:], model1_feats_imp_scores))
+  model2_name_score_pairs = list(zip(df.columns[1:], model2_feats_imp_scores))
+
+  model1_name_score_pairs = sorted(model1_name_score_pairs, key=lambda tup: tup[1], reverse = True)
+  model2_name_score_pairs = sorted(model2_name_score_pairs, key=lambda tup: tup[1], reverse = True)
+
+  model2_names, model2_scores = zip(*model2_name_score_pairs)
+ 
+  model1_scores = [dict(model1_name_score_pairs)[name] for name in model2_names]
+
+  model2_names = model2_names[:num_feats_to_plot]
+
+  model1_scores = model1_scores[:num_feats_to_plot]
+  model2_scores = model2_scores[:num_feats_to_plot]
+
+  x_range = np.array(range(len(model2_names)))
+
+  plt.yticks(fontsize = 15)
+  plt.ylabel("Relative importance score", fontsize = 18)
+  plt.bar(x_range, model2_scores, width = 0.4, color = 'red')
+  plt.xticks(x_range, model2_names, rotation = 90, fontsize = 16)
+
+  plt.bar(x_range + 0.4, model1_scores, width = 0.4, color = 'blue')
+  
+  plt.legend(["Randon Forest", "Ada Boost"], fontsize = 16)
+  plt.savefig(logger.get_output_file(plot_filename), dpi = 120, bbox_inches='tight')
+  plt.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
